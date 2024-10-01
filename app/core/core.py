4815c635f79c5b91c44bc7ff7f700ed99596c590
Ujwal N K <ujwal.nk@gmail.com>
2024-10-01 13:16:03 +0530

    Unauth header & timeslot for wait

--- app/core/core.py
+++ app/core/core.py
@@ -124,8 +124,8 @@ async def login_for_access_token(
     ):
 
         raise HTTPException(
-            status_code=201,
-            detail="{'message': 'Wait your turn', 'timeslot_start': user.start_time, 'timeslot_end': user.end_time}",
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={'message': 'Wait your turn', 'timeslot_start': user.start_time, 'timeslot_end': user.end_time},
             headers={"WWW-Authenticate": "Bearer"},
         )
 
